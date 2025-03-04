6f5c30956ead167e0e3cbe63428a3bb04c354f5e
DICREY <dicreyraptor@gmail.com>
2025-03-04 08:19:53 -0500

    1.1.1

---
+++ src/module/App.jsx
@@ -0,0 +1,13 @@
+import FormularioRegistro from './Componentes/FormularioRegistro';
+import RegistroMascota from './Componentes/FormularioMascotas';
+
+function App() {
+  return (
+    <div className="App">
+      <FormularioRegistro/>
+      {/* <RegistroMascota/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/module/login.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const LoginForm = () => {
+
+    return (
+        <form onSubmit={hand}>
+            <span>
+                <p></p>
+                <input type="text" name="" id="" />
+            </span>
+            <span>
+                <p></p>
+                <input type="text" name="" id="" />
+            </span>
+            <span>
+                <a href="#">¿Forgot you password?</a>
+            </span>
+        </form>
+    )
+}
