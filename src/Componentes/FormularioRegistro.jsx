6c9e2ca21d5ed128f7038c0a6d8a6353a5467198
Mogom <kevin_spalacios@soy.sena.edu.co>
2025-03-04 10:55:11 -0500

    Foooter2

--- src/Componentes/FormularioRegistro.jsx
+++ src/Componentes/FormularioRegistro.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
+import "../../public/styles/formulario-registro.css"
 
 const FormularioRegistro = () => {
   const [paso, setPaso] = useState(1)
