fa94fa4bbbef03dc722014c4d3587e7603e92ea1
Mogom <kevin_spalacios@soy.sena.edu.co>
2025-03-04 10:50:32 -0500

    Footer2

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,27 +1,46 @@
 import React from "react";
+import "../../public/styles/footer.css"
 
-export default function Footer(){
+
+const Footer = () => {
     return (
-        <footer className="main-container">
-            <div className="left-container">
-                <img src={require(`../../public/imgs/logoTitle.jpg`)} alt="Logo PetsHeaven" />
+        <main className="main-container">
+            <div className="data-container">
+                <img src="../../public/imgs/1.png" alt="Logo PetsHeaven" />
                 <section className="redes">
-
+                    <ul className="canales">
+                        <li className="social">1</li>
+                        <li className="social">2</li>
+                        <li className="social">3</li>
+                        <li className="social">4</li>
+                    </ul>
                 </section>
                 <p className="fecha-act">
-
+                    2025
                 </p>
             </div>
-            <div className="center-container">
+            <div className="data-container">
                 <h3 className="title-conocenos">Conocenos</h3>
                 <a className="parra-conocenos">Linea de atencion</a>
-                <a className="parra-conocenos"></a>
-                <a className="parra-conocenos"></a>
-                <a className="parra-conocenos"></a>
+                <a className="parra-conocenos">Cel 32131231212/Whatsapp</a>
+                <a className="parra-conocenos">Linea Gratuita Nacional</a>
+                <a className="parra-conocenos">petsHeaven@sena.edu.co</a>
+                <a className="parra-conocenos">Trabaje con nosotros</a>
+                <a className="parra-conocenos">Ingreso corporativo</a>
             </div>
-            <div className="right-container">
-
+            <div className="data-container">
+                <h3 className="title-conocenos">Terminos y condiciones</h3>
+                <a className="parra-conocenos">Quienes somos</a>
+                <a className="parra-conocenos">Nuestra historia</a>
+                <a className="parra-conocenos">Sedes</a>
+                <a className="parra-conocenos">Contactenos</a>
+                <a className="parra-conocenos">Preguntas Frecuentes</a>
+                <a className="parra-conocenos">Politcas de cambio y garantia</a>
+                <a className="parra-conocenos">Politica de tratamiento de datos</a>
+                <a className="parra-conocenos">Area Legal</a>
             </div>
-        </footer>
+        </main>
     );
 }
+
+export default Footer;
