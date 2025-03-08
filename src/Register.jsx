651a3fd106e336e299601a5e8ce1cd248a6eef8e
Dicrey <dicreyraptor@gmail.com>
2025-03-08 06:46:35 -0500

    1.1.7

--- src/Register.jsx
+++ src/Register.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client'
 // Imports
 import FormularioRegistro from "./Componentes/FormularioRegistro.jsx"
 import Footer from "./Componentes/Footer.jsx"
-import "../../public/styles/formulario-registro.css"
+import "../public/styles/formulario-registro.css"
 
 // Create root
 const root = ReactDOM.createRoot(document.getElementById('root'))
