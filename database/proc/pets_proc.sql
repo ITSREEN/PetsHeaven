94622b172b5e68afb28e29a1499c6387072a7620
Dicrey <dicreyraptor@gmail.com>
2025-04-01 19:29:13 -0500

    1.1.33

--- database/proc/pets_proc.sql
+++ database/proc/pets_proc.sql
@@ -6,6 +6,7 @@ CREATE PROCEDURE pets_heaven.RegistPets(
     IN p_ali_mas VARCHAR(100),
     IN p_fec_nac_mas DATE,
     IN p_pes_mas FLOAT,
+    IN p_usuario VARCHAR(100),
     IN p_gen_mas VARCHAR(2),
     IN p_est_rep_mas VARCHAR(100),
     IN p_fot_mas TEXT
@@ -22,7 +23,15 @@ BEGIN
 
     START TRANSACTION;
 
-    INSERT INTO
+    SELECT 1 FROM usuarios u 
+    WHERE 
+        u.doc_usu LIKE p_usuario 
+        OR u.email_usu LIKE p_usuario;
+
+    SET p_id_pro_mas = LAST_INSERT_ID();
+
+    INSERT INTO pets_heaven.mascotas (nom_mas,esp_mas,col_mas,raz_mas,ali_mas,fec_nac_mas,pes_mas,gen_mas,id_pro_mas,est_rep_mas,fot_mas)
+    VALUES(p_nom_mas,p_esp_mas,p_col_mas,p_raz_mas,p_ali_mas,p_fec_nac_mas,p_pes_mas,p_gen_mas,p_id_pro,p_est_rep_mas,p_fot_mas);
 
     COMMIT;
     SET autocommit = 1;
--- database/registros.sql
+++ database/registros.sql
@@ -42,3 +42,16 @@ INSERT INTO pets_heaven.servicios (cat_ser, nom_ser, pre_ser, des_ser, tec_des_s
 (2, 'Cirugía', 200.00, 'Procedimientos quirúrgicos realizados por especialistas con equipos de última generación.', 'Evaluación pre-quirúrgica, anestesia monitorizada, equipo esterilizado, recuperación asistida.', 'https://media.githubusercontent.com/media/Mogom/Imagenes_PetsHeaven/main/Servicios/cirugia.png', 1),
 (3, 'Emergencias 24h', 80.00, 'Atención inmediata para situaciones urgentes a cualquier hora del día.', 'Equipo de emergencias disponible 24/7, unidad de cuidados intensivos, monitoreo constante.', 'https://media.githubusercontent.com/media/Mogom/Imagenes_PetsHeaven/main/Servicios/urgencias.png', 1),
 (4, 'Spa y Baño', 45.00, 'Servicio completo de baño, corte de pelo, limpieza de oídos y corte de uñas para tu mascota.', 'Baño con productos hipoalergénicos, secado profesional, corte según raza, cuidado de uñas y oídos.', 'https://media.githubusercontent.com/media/Mogom/Imagenes_PetsHeaven/main/Servicios/ba%C3%B1o.png', 1);
+
+INSERT INTO pets_heaven.mascotas 
+(nom_mas, esp_mas, col_mas, raz_mas, ali_mas, fec_nac_mas, pes_mas, gen_mas, id_pro_mas, est_rep_mas, fot_mas) VALUES
+('Max', 'Perro', 'Marrón', 'Labrador Retriever', 'Purina Dog Chow', '2018-05-15', 28.5, 'M', 1, 'No esterilizado', 'max_labrador.jpg'),
+('Luna', 'Gato', 'Negro', 'Siamés', 'Whiskas', '2019-11-22', 4.2, 'F', 2, 'Esterilizado', 'luna_siames.jpg'),
+('Rocky', 'Perro', 'Blanco', 'Bulldog Francés', 'Royal Canin', '2020-03-10', 12.7, 'M', 3, 'No esterilizado', 'rocky_bulldog.jpg'),
+('Bella', 'Gato', 'Gris', 'Persa', 'Hill\'s Science Diet', '2017-07-30', 5.1, 'F', 4, 'Esterilizado', 'bella_persa.jpg'),
+('Charlie', 'Perro', 'Dorado', 'Golden Retriever', 'Pedigree', '2019-01-25', 32.0, 'M', 5, 'No esterilizado', 'charlie_golden.jpg'),
+('Milo', 'Gato', 'Atigrado', 'Mestizo', 'Friskies', '2021-02-14', 3.8, 'M', 1, 'No esterilizado', 'milo_atigrado.jpg'),
+('Lucy', 'Perro', 'Blanco y Negro', 'Border Collie', 'Eukanuba', '2016-09-05', 20.3, 'F', 2, 'Esterilizado', 'lucy_collie.jpg'),
+('Oliver', 'Gato', 'Naranja', 'Maine Coon', 'Blue Buffalo', '2018-12-18', 7.5, 'M', 3, 'Esterilizado', 'oliver_maine.jpg'),
+('Daisy', 'Perro', 'Crema', 'Poodle', 'Nutro', '2020-06-08', 6.8, 'F', 4, 'No esterilizado', 'daisy_poodle.jpg'),
+('Leo', 'Perro', 'Negro y Fuego', 'Doberman', 'Canidae', '2017-04-20', 34.2, 'M', 5, 'Esterilizado', 'leo_doberman.jpg');
