e33a86f8534e8d22a953e5923351e6d53bd2648d
Mogom <kevin_spalacios@soy.sena.edu.co>
2025-04-08 11:30:48 -0500

    1.1.60

--- prueba.js
+++ prueba.js
@@ -1,21 +1,34 @@
-async function GetData(URL) {
+async function PostData(URL,datas) {
     try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-            response.status === 500? window.location.href = "/internal":
-            window.location.href = "/notfound"
-            throw new Error(`HTTP error! status: ${response.status}`);
+        const response = await fetch(URL,{
+            method:"POST",
+            body: datas,
+        })
+        if (!response.status === 201) {
+            // response.status >= 500? window.location.href = "/internal":
+            // window.location.href = "/notfound"
+            throw new Error(`HTTP error! status: ${response.status}`)
         }
-        const data = await response.json();
-        console.log(data.result[0])
-        return data.result[0];
+        const data = await response
+        return data.result
     } catch (error) {
-        console.error("Error:", error);
-        throw error;
-    } finally {
-        console.log("Procedimiento Terminado");
+        // window.location.href = "/internal"
+        console.error("Error:", error)
+        throw error
     }
 }
 
-let datas = GetData("http://localhost:3000/user/all")
-console.log(datas)
+let datas = await PostData("http://localhost:3000/user/register",{
+    apellidos: "Perra",
+    celular: "1231312331",
+    celular2: "",
+    codigoVerificacion: "501261",
+    direccion: "dfdfsdf",
+    email: "kevinstivenasd04@gmail.com",
+    fechaNacimiento: "2002-12-09",
+    genero: "F",
+    nombres: "Juana",
+    numeroDocumento: "2256652254",
+    password:"Unaperra123@",
+    tipoDocumento:"CC"
+})
